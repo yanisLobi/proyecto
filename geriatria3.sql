ef953bb870ee9404568a2e1d3644f5eb3b3cd47b
Yanet Laz <lazarocamposyanet1203@gmail.com>
2026-07-26 23:18:06 -0600

    actualizacion de base de datos

--- geriatria3.sql
+++ geriatria3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-07-2026 a las 04:07:22
+-- Tiempo de generación: 27-07-2026 a las 07:17:04
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -33,16 +33,17 @@ CREATE TABLE `medicamentos` (
   `me_forma_farmaceutica` enum('Solidas','Semisolidas','Liquidas','Inhalables') DEFAULT NULL,
   `me_concentracion` varchar(50) NOT NULL,
   `me_fecha_caducidad` date NOT NULL,
-  `me_descripcion` varchar(100) DEFAULT NULL
+  `me_descripcion` varchar(100) DEFAULT NULL,
+  `me_activo` tinyint(4) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `medicamentos`
 --
 
-INSERT INTO `medicamentos` (`id_medicamentos`, `me_nombre_comercial`, `me_forma_farmaceutica`, `me_concentracion`, `me_fecha_caducidad`, `me_descripcion`) VALUES
-(1, 'Paracetamol', 'Solidas', '500mg', '2027-12-31', 'Analgésico'),
-(2, 'Amoxicilina', 'Liquidas', '250mg/5ml', '2026-06-30', 'Recomedable.');
+INSERT INTO `medicamentos` (`id_medicamentos`, `me_nombre_comercial`, `me_forma_farmaceutica`, `me_concentracion`, `me_fecha_caducidad`, `me_descripcion`, `me_activo`) VALUES
+(1, 'Paracetamol', 'Solidas', '500mg', '2027-12-31', 'Analgésico', 1),
+(2, 'Amoxicilina', 'Liquidas', '250mg/5ml', '2026-06-30', 'Recomedable.', 0);
 
 -- --------------------------------------------------------
 
@@ -57,16 +58,18 @@ CREATE TABLE `pacientes` (
   `pa_fecha_nacimiento` date NOT NULL,
   `pa_nombre_contacto_emergencia` varchar(50) NOT NULL,
   `pa_tel_contacto_emergencia` bigint(10) NOT NULL,
-  `id_enfermera_principal` int(11) NOT NULL
+  `id_enfermera_principal` int(11) NOT NULL,
+  `pa_activo` tinyint(4) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `pacientes`
 --
 
-INSERT INTO `pacientes` (`id_pacientes`, `pa_nombre`, `pa_apellidos`, `pa_fecha_nacimiento`, `pa_nombre_contacto_emergencia`, `pa_tel_contacto_emergencia`, `id_enfermera_principal`) VALUES
-(9, 'karla', 'sosa', '2026-07-16', 'sepa', 1122334455, 19),
-(10, 'Monse', 'Campusano Juarez', '2019-07-18', 'sosa', 1122334455, 19);
+INSERT INTO `pacientes` (`id_pacientes`, `pa_nombre`, `pa_apellidos`, `pa_fecha_nacimiento`, `pa_nombre_contacto_emergencia`, `pa_tel_contacto_emergencia`, `id_enfermera_principal`, `pa_activo`) VALUES
+(9, 'karla', 'Medina', '2026-07-24', 'Eli', 1122334455, 17, 1),
+(10, 'Monse', 'Campusano Juarez', '2026-07-24', 'Juan', 1122334455, 19, 0),
+(11, 'Emili', 'Garcia Lopes', '2026-07-01', 'Paloma juarez', 2233445566, 17, 0);
 
 -- --------------------------------------------------------
 
@@ -93,8 +96,16 @@ CREATE TABLE `tratamientos` (
   `id_doctor` int(11) NOT NULL,
   `tr_fecha_inicio` date NOT NULL,
   `tr_fecha_final` date NOT NULL,
-  `tr_descripcion` varchar(100) NOT NULL
+  `tr_descripcion` varchar(100) NOT NULL,
+  `tr_activo` tinyint(4) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `tratamientos`
+--
+
+INSERT INTO `tratamientos` (`id_tratamientos`, `tr_nombre`, `id_paciente`, `id_doctor`, `tr_fecha_inicio`, `tr_fecha_final`, `tr_descripcion`, `tr_activo`) VALUES
+(6, 'Alzheimer', 10, 18, '2026-07-25', '2026-07-30', 'Precaución ', 1);
 
 -- --------------------------------------------------------
 
@@ -112,24 +123,23 @@ CREATE TABLE `usuarios` (
   `us_telefono` bigint(10) NOT NULL,
   `us_correo_electronico` varchar(50) NOT NULL,
   `us_direccion` varchar(200) NOT NULL,
-  `us_especialidad` enum('Geriatría',',Médico General') DEFAULT NULL
+  `us_especialidad` enum('Geriatría',',Médico General') DEFAULT NULL,
+  `us_activo` tinyint(4) NOT NULL DEFAULT 1,
+  `us_cedula` int(11) NOT NULL DEFAULT 10000000
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id_usuarios`, `us_tipo_usuario`, `us_nombre`, `us_apellidos`, `us_fecha_nacimiento`, `us_contraseña`, `us_telefono`, `us_correo_electronico`, `us_direccion`, `us_especialidad`) VALUES
-(1, 'Doctor', 'Juan', 'Morales Santiago', '2060-05-12', '12345', 1234567890, 'juan@gmail.com', 'Calle Falsa 123', 'Geriatría'),
-(7, 'Enfermera', 'Camila', 'Santiago Juárez', '2091-07-17', '12345', 2147483647, 'camila@gmail.com', 'Calle 1 de enero', ''),
-(15, 'Doctor', 'Camila', 'Camacho', '2026-07-15', '12345', 4455667788, 'camila', 'calle 1 de enero', 'Geriatría'),
-(16, 'Administrador', 'Pedro', 'García', '2026-07-15', '12345', 4455667788, 'pedro@garcia', 'calle falsa', 'Geriatría'),
-(17, 'Enfermera', 'Juan carlos', 'Lopez velazsquez', '2026-07-08', '12345', 1122334455, 'carlos@gamil.com', 'calle falsa', ''),
-(18, 'Doctor', 'Paola', 'Montayo Nose', '2026-07-15', '12345', 5537196729, 'paola@montayo', 'calle falsa', 'Geriatría'),
-(19, 'Enfermera', 'Manuel', 'Santiago Garcia', '2091-07-18', '12345', 1122334455, 'santi@gmail', 'calle 1 de enero ', ''),
-(20, 'Doctor', 'a', 'a', '2026-07-16', 'a', 1, 'a', 'a1', ',Médico General'),
-(22, 'Enfermera', 'e', 'e', '2026-07-16', 'e', 2, 'e', 'e1', ',Médico General'),
-(23, 'Administrador', 'b', 'b', '2026-07-16', 'b', 1, 'b', 'b1', 'Geriatría');
+INSERT INTO `usuarios` (`id_usuarios`, `us_tipo_usuario`, `us_nombre`, `us_apellidos`, `us_fecha_nacimiento`, `us_contraseña`, `us_telefono`, `us_correo_electronico`, `us_direccion`, `us_especialidad`, `us_activo`, `us_cedula`) VALUES
+(7, 'Enfermera', 'Camila', 'Santiago Juárez', '2091-07-17', '12345', 2147483647, 'camila@gmail.com', 'Calle 1 de enero', '', 1, 10000000),
+(15, 'Doctor', 'Camila', 'Camacho', '2026-07-15', '12345', 4455667788, 'camila', 'calle 1 de enero', 'Geriatría', 1, 10000000),
+(16, 'Administrador', 'Pedro', 'García', '2026-07-15', '12345', 4455667788, 'pedro@garcia', 'calle falsa', 'Geriatría', 1, 10000000),
+(17, 'Enfermera', 'Juan carlos', 'Lopez velazsquez', '2026-07-08', '12345', 1122334455, 'carlos@gamil.com', 'calle falsa', '', 1, 10000000),
+(18, 'Doctor', 'Paola', 'Montayo Nose', '2026-07-15', '12345', 5537196729, 'paola@montayo', 'calle falsa', 'Geriatría', 1, 10000000),
+(19, 'Enfermera', 'Manuel', 'Santiago Garcia', '2091-07-18', '12345', 1122334455, 'santi@gmail', 'calle 1 de enero ', '', 1, 10000000),
+(20, 'Doctor', 'Juan', 'camacho', '2000-12-02', '12345', 1122334455, 'juan@camacho', '2 de octubre', '', 1, 10000000);
 
 --
 -- Índices para tablas volcadas
@@ -184,7 +194,7 @@ ALTER TABLE `medicamentos`
 -- AUTO_INCREMENT de la tabla `pacientes`
 --
 ALTER TABLE `pacientes`
-  MODIFY `id_pacientes` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_pacientes` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `receta`
@@ -196,13 +206,13 @@ ALTER TABLE `receta`
 -- AUTO_INCREMENT de la tabla `tratamientos`
 --
 ALTER TABLE `tratamientos`
-  MODIFY `id_tratamientos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_tratamientos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuarios` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id_usuarios` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- Restricciones para tablas volcadas
